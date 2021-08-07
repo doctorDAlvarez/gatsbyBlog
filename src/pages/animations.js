1d4084407f4fe0ed139ea7963696d8822150d81c
Diego Alvarez <doctordalvarez@protonmail.com>
2021-08-06 23:13:42 -0300

    animations

---
+++ src/pages/animations.js
@@ -0,0 +1,30 @@
+import { relativeTimeRounding } from "moment";
+import React from "react";
+import Layout from "../components/layout";
+import video from "../images/screen.mp4";
+import snippet from "../images/code.png";
+
+export default function Animations() {
+  return (
+    <div>
+      <Layout>
+        <h4>react-spring library</h4>
+        <div
+          style={{
+            display: "flex",
+            margin: "auto",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <video width="300" height="300" src={video} controls></video>
+          </div>
+          <div>
+            <img width="300" height="300" src={snippet} alt="a" />
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,22 @@
-import { Link } from 'gatsby';
-import React from 'react'
-import styled from 'styled-components';
-import Article from '../components/article';
-import Layout from '../components/layout';
-import resumeFile from '../images/Profile.pdf';
-
+import { Link } from "gatsby";
+import React from "react";
+import styled from "styled-components";
+import Article from "../components/article";
+import Layout from "../components/layout";
+import resumeFile from "../images/Profile.pdf";
 
 const AsideStyle = styled.div`
   display: flex;
   justify-content: flex-start;
   margin-bottom: 5rem;
-  color: ${props => props.theme.dark ? "antiquewhite" : "#494868"};;
+  color: ${(props) => (props.theme.dark ? "antiquewhite" : "#494868")};
   word-wrap: break-word;
   font-kerning: normal;
   font-size: smaller;
-  transition: all .6s ease;
+  transition: all 0.6s ease;
   a {
     text-decoration: none;
-    color: ${props => props.theme.dark ? "lightgreen" : "#28acb4"};;
+    color: ${(props) => (props.theme.dark ? "lightgreen" : "#28acb4")};
   }
   p {
     display: block;
@@ -37,11 +36,10 @@ const LinkStyle = styled.div`
   text-align: center;
   margin-top: 80px;
   h3 {
-    color: ${props => props.theme.dark ? "lightgreen" : "lightcoral"};;
+    color: ${(props) => (props.theme.dark ? "lightgreen" : "lightcoral")};
   }
   a {
     text-decoration: none;
-
   }
 `;
 const MainStyle = styled.main`
@@ -55,39 +53,49 @@ const MainStyle = styled.main`
   }
 `;
 
-
 const IndexPage = () => {
-  
   return (
-  <div>
-    <Layout>
-      <aside>
-      <AsideStyle>
-        <ProfileImgStyle src="https://pbs.twimg.com/profile_images/1389558491245944835/7V71O-cX_400x400.jpg" alt="profile"></ProfileImgStyle>
-        <p style={{display: 'block', marginLeft: 17}}>
-          Shared code and ideas by <a href="https://www.twitter.com/doctordalvarez">@Diego Alvarez
+    <div>
+      <Layout>
+        <aside>
+          <AsideStyle>
+            <ProfileImgStyle
+              src="https://pbs.twimg.com/profile_images/1389558491245944835/7V71O-cX_400x400.jpg"
+              alt="profile"
+            ></ProfileImgStyle>
+            <p style={{ display: "block", marginLeft: 17 }}>
+              Shared code and ideas by{" "}
+              <a href="https://www.twitter.com/doctordalvarez">
+                @Diego Alvarez
+              </a>
+              .<br></br>Developer, <i>thinking</i> in React.
+            </p>
+          </AsideStyle>
+        </aside>
+        <MainStyle>
+          <a href="https://gist.github.com/doctorDAlvarez">
+            <Article title="Code" subheading="components - hooks" />
           </a>
-          .
-          <br></br>Developer, <i>thinking</i> in React.    
-        </p>
-      </AsideStyle>
-      </aside>
-      <MainStyle>
-      <a href='https://gist.github.com/doctorDAlvarez'><Article title="<code />" subheading="components - hooks"/></a>
-        <Link to='/projects'> <Article title="<work />" subheading="projects"/> </Link>
-        <a href='https://doctordalvarez.hashnode.dev/'><Article title="<posts />" subheading="soft-skills"/></a>
-        <a href='https://gist.github.com/doctorDAlvarez'><Article title="<community />" subheading=" (🛠) pr / contributions"/></a>
-
-      </MainStyle>
-      <LinkStyle>
-          <a href={resumeFile} download><h3>Resume</h3></a>
+          <Link to="/projects">
+            {" "}
+            <Article title="Work" subheading="projects" />{" "}
+          </Link>
+          <a href="https://doctordalvarez.hashnode.dev/">
+            <Article title="Social" subheading="soft-skills" />
+          </a>
+          <Link to="/animations">
+            <Article title="Animations" subheading=" (🛠) Spring" />
+          </Link>
+        </MainStyle>
+        <LinkStyle>
+          <a href={resumeFile} download>
+            <h3>Resume</h3>
+          </a>
         </LinkStyle>
-    </Layout>
-
-  </div>
-  
-  )
-}
+      </Layout>
+    </div>
+  );
+};
 
 // Step 3: Export your component
-export default IndexPage
+export default IndexPage;
